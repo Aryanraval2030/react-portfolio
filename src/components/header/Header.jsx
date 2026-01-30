cd4f5bc3a506c2c0d7a6fdfc954ba7a56541481d
aryan raval <aryanraval1432@gmail.com.>
2026-01-30 12:39:03 +0530

    my-portfolio

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold text-blue-500 underline">Hello World</h1>
+    </div>
+  )
+}
+
+export default Header
